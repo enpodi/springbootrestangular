a3e7db3e2d76cbfc5a7e1914f223f31dbb592d22
unknown <enpodi@yahoo.com.mx>
2018-07-24 00:12:31 -0700

    component and service added

--- crud-ui/src/app/app.module.ts
+++ crud-ui/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CrudService } from './services/crud.service';
+import { AdminComponent } from './components/admin/admin.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HttpClientModule,
+    AdminComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [CrudService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ crud-ui/src/app/components/admin/admin.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../../services/crud.service';
+
+@Component({
+  selector: 'app-admin',
+  template: 'admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+  public bikes;
+
+  constructor(private crudService: CrudService) { }
+
+  ngOnInit() {
+    this.getBikes();
+  }
+
+  getBikes(){
+    this.crudService.getBikes().subscribe(
+      data => { this.bikes = data},
+      err => console.error(err),
+      () => console.log('bikes loaded')
+    );
+  }
+
+}
